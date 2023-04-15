2964ebdb9de4697a36cb4752c7afdc72390ee6d8
amanb87 <abuttan87@gmail.com>
2023-04-15 17:09:56 +0530

    remove unnecessary files

--- functest.py
+++
@@ -1,8 +0,0 @@
-import astroscrappy
-from astropy.io import fits
-
-def a_test(*args):        
-    path = args[0]   
-    s = fits.getdata(path)
-    a = astroscrappy.detect_cosmics(s)
-    return [a,path]
